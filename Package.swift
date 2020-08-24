f6fdc1e031ea51048089733fbe1b16edfa745cd2
Alexey Korolev <alphatroya@gmail.com>
2020-08-24 13:55:59 +0300

    Update argument parser library (#8)

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .executable(name: "redmine-cli", targets: ["RedmineCLI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.2.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable", from: "0.9.0"),
     ],
     targets: [
