c96a67208e0779ae5e920c1565c744e7ffb95f92
Alexey Korolev <alphatroya@gmail.com>
2021-03-01 12:27:25 +0300

    Add pagination support for projects request (#17)

--- Sources/Redmine/Endpoints/Project/ProjectsEndpoint.swift
+++ Sources/Redmine/Endpoints/Project/ProjectsEndpoint.swift
@@ -6,11 +6,21 @@
 import Foundation
 
 struct ProjectsEndpoint: RequestTarget, EndpointDescription {
+    let offset: Int
+    let limit: Int
+
     var method: Method {
         .get
     }
 
     var path: String {
         "projects"
+    }
+
+    var queryItems: [URLQueryItem] {
+        [
+            .init(name: "limit", value: "\(limit)"),
+            .init(name: "offset", value: "\(offset)"),
+        ]
     }
 }
